2d442dff72f1285125e14ee29532dba8df8634db
BJak22 <bartos.j.2002@gmail.com>
2024-08-21 12:09:13 +0200

    pdf handling

--- database.py
+++ database.py
@@ -13,8 +13,6 @@ db_name = os.getenv('DB_NAME', 'StudentManagement')
 databaseURL = f'mysql+pymysql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}'
 
 
-#'mysql+pymysql://root:@localhost:3306/StudentManagement'
-
 engine = create_engine(databaseURL)
 
 sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
