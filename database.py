8a5f63fb166bc1ed63e0de5b56de6a4591f78150
BJak22 <bartos.j.2002@gmail.com>
2024-08-27 11:59:56 +0200

    Add Procfile and runtime.txt for Heroku deployment

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ db_username = os.getenv('DB_USERNAME', 'your_clearDB_username')
 db_password = os.getenv('DB_PASSWORD', 'your_clearDB_password')
 # DB_HOST for testing
 #db_host = os.getenv('DB_HOST', 'localhost')
-db_host = os.getenv('DB_HOST', 'your_clearDB_host')  # Przykład: us-cdbr-east-05.cleardb.net
+db_host = os.getenv('DB_HOST', 'your_clearDB_host')
 db_port = os.getenv('DB_PORT', '3306')
 db_name = os.getenv('DB_NAME', 'your_clearDB_dbname')
 
