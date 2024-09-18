70e9463504d81c5e922de2004e0f0c48aaf2d5ac
BJak22 <128638783+BJak22@users.noreply.github.com>
2024-09-18 08:57:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ import boto3
 #AWS S3 Configuration
 s3_bucket = os.getenv('S3_BUCKET', 'bjak22studentmanagementapibucket')
 s3_region = os.getenv('S3_REGION', 'us-east-1')
-aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID', 'AKIAYM7PN65OG232CT2I')
-aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY', '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')
+aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID', 'Key')
+aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY', 'secretKey')
 s3_client = boto3.client(
     's3',
     region_name=s3_region,
